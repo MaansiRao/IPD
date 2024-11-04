9617bdd895198717857202809b2ccc7a62e3563b
Maansi Rao <143645224+MaansiRao@users.noreply.github.com>
2024-11-04 22:17:09 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import AACBoard from "./AACBoard"
+
+function App() {
+  return (
+    <>
+    {/* <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1> */}
+    <AACBoard/>
+    </>
+  )
+}
+
+export default App
