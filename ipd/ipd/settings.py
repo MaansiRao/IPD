b9792006b31cfc95d215055027ad655542064343
NT3112 <nehatendulkar3112@gmail.com>
2025-04-20 20:53:54 +0530

    RenderPostgres

--- ipd/ipd/settings.py
+++ ipd/ipd/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from decouple import config
 import os
 from datetime import timedelta
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -106,11 +107,14 @@ WSGI_APPLICATION = 'ipd.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+  
+    'default': dj_database_url.parse(
+        config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
     }
-}
+
 
 
 # Password validation
